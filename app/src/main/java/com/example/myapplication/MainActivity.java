7227e859cae5397b982de55fe3f0edfc83457f87
darli <darlinmanuelc05@gmail.com>
2023-03-29 21:04:30 -0400

    Pre-Set adapter

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    //1- Adapterview
+    ListView listview;
+
+    //2- Creating the data Source
+    String[] worldCup22 = {
+            "Germany",
+            "Spain",
+            "Brazil",
+            "England",
+            "USA",
+            "Saudi Arabia",
+            "Qatar"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listview = findViewById(R.id.listview);
+
+        //3- Adapter
+        ArrayAdapter adapter= new ArrayAdapter(this, android.R.layout.simple_list_item_1, worldCup22);
+
+        listview.setAdapter(adapter);
+    }
+}
